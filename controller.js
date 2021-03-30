4aeb92e457f177b0ea03f008a2270179037117dd
ilham abdul aziz <azizyeea@ilhams-MacBook-Air.local>
2021-03-30 17:49:14 +0700

    berhasil tambah data

--- controller.js
+++ controller.js
@@ -2,30 +2,47 @@
 var response = require('./res');
 var connection = require('./koneksi');
 
-exports.index = function(req,res){
-    response.ok("Aplikasi REST API ku berjalan!",res)
+exports.index = function (req, res) {
+    response.ok("Aplikasi REST API ku berjalan!", res)
 };
 
 //menampilkan semua data mahasiswa
-exports.tampilsemuamahasiswa = function(req,res){
-    connection.query('SELECT * FROM mahasiswa', function(error,rows,field){
-        if(error){
+exports.tampilsemuamahasiswa = function (req, res) {
+    connection.query('SELECT * FROM mahasiswa', function (error, rows, field) {
+        if (error) {
             console.log(error);
-        }else{
-            response.ok(rows,res);
+        } else {
+            response.ok(rows, res);
         }
     });
 };
 
 //menampilkan semua data mahasiswa bardasarkan id
-exports.tampilberdasarkanid = function(req,res){
+exports.tampilberdasarkanid = function (req, res) {
     let id = req.params.id;
     connection.query('SELECT * FROM mahasiswa WHERE id_mahasiswa = ?', [id],
-    function(error, rows, field){
-        if(error){
-            console.log(error);
-        }else{
-            response.ok(rows,res);
-        }
-    });
+        function (error, rows, field) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok(rows, res);
+            }
+        });
+};
+
+//menambahkan data mahasiswa
+exports.tambahmahasiswa = function (req, res) {
+    var nim = req.body.nim;
+    var nama = req.body.nama;
+    var jurusan = req.body.jurusan;
+
+    connection.query('INSERT INTO mahasiswa (nim,nama,jurusan) VALUES(?,?,?)',
+        [nim, nama, jurusan],
+        function (error, rows, fields){
+            if(error){
+                console.log(error);
+            }else{
+                response.ok("Berhasil Menambahkan Data !",res)
+            }
+        });
 };
