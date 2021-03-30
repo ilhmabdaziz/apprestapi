bf28c99e4161a768f37b14400f6aebecf7e99e55
ilham abdul aziz <azizyeea@ilhams-MacBook-Air.local>
2021-03-30 22:31:49 +0700

    registrasi berhasil

---
+++ middleware/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+var auth = require('./auth');
+var router = express.Router();
+
+//daftarkan menu registrasi
+router.post('/api/v1/register', auth.registrasi);
+
+module.exports = router;
