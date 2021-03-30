196fa50584350626454ca5b6e995e53bc06df18d
ilham abdul aziz <azizyeea@ilhams-MacBook-Air.local>
2021-03-30 23:41:53 +0700

    oke role beres

--- middleware/index.js
+++ middleware/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 var auth = require('./auth');
+// const verifikasi = require('./verifikasi');
 var router = express.Router();
+var verifikasi = require('./verifikasi');
 
 //daftarkan menu registrasi
 router.post('/api/v1/register', auth.registrasi);
 router.post('/api/v1/login', auth.login);
+
+//alamat yang perlu otorisasi
+router.get('/api/v1/rahasia', verifikasi(), auth.halamanrahasia);
 
 module.exports = router;
