d94a9b2d7059e209dd1a9fc6818396ae8dafa31e
ilhmabdaziz <ilhmabdaziz@yahoo.com>
2021-10-04 13:34:09 +0700

    finish

--- controller.js
+++ controller.js
@@ -1,92 +1,104 @@
-'use strict';
-var response = require('./res');
-var connection = require('./koneksi');
+"use strict";
+var response = require("./res");
+var connection = require("./koneksi");
 
 exports.index = function (req, res) {
-    response.ok("Aplikasi REST API ku berjalan!", res)
+  response.ok("Aplikasi REST API ku berjalan!", res);
 };
 
 //menampilkan semua data mahasiswa
 exports.tampilsemuamahasiswa = function (req, res) {
-    connection.query('SELECT * FROM mahasiswa', function (error, rows, field) {
-        if (error) {
-            console.log(error);
-        } else {
-            response.ok(rows, res);
-        }
-    });
+  connection.query("SELECT * FROM mahasiswa", function (error, rows, field) {
+    if (error) {
+      console.log(error);
+    } else {
+      response.ok(rows, res);
+    }
+  });
 };
 
 //menampilkan semua data mahasiswa bardasarkan id
 exports.tampilberdasarkanid = function (req, res) {
-    let id = req.params.id;
-    connection.query('SELECT * FROM mahasiswa WHERE id_mahasiswa = ?', [id],
-        function (error, rows, field) {
-            if (error) {
-                console.log(error);
-            } else {
-                response.ok(rows, res);
-            }
-        });
+  let id = req.params.id;
+  connection.query(
+    "SELECT * FROM mahasiswa WHERE id_mahasiswa = ?",
+    [id],
+    function (error, rows, field) {
+      if (error) {
+        console.log(error);
+      } else {
+        response.ok(rows, res);
+      }
+    }
+  );
 };
 
 //menambahkan data mahasiswa
 exports.tambahmahasiswa = function (req, res) {
-    var nim = req.body.nim;
-    var nama = req.body.nama;
-    var jurusan = req.body.jurusan;
+  var nim = req.body.nim;
+  var nama = req.body.nama;
+  var jurusan = req.body.jurusan;
 
-    connection.query('INSERT INTO mahasiswa (nim,nama,jurusan) VALUES(?,?,?)',
-        [nim, nama, jurusan],
-        function (error, rows, fields) {
-            if (error) {
-                console.log(error);
-            } else {
-                response.ok("Berhasil Menambahkan Data !", res)
-            }
-        });
+  connection.query(
+    "INSERT INTO mahasiswa (nim,nama,jurusan) VALUES(?,?,?)",
+    [nim, nama, jurusan],
+    function (error, rows, fields) {
+      if (error) {
+        console.log(error);
+      } else {
+        response.ok("Berhasil Menambahkan Data !", res);
+      }
+    }
+  );
 };
 
 //mengubah data berdasarkan id
 exports.ubahmahasiswa = function (req, res) {
-    var id = req.body.id_mahasiswa;
-    var nim = req.body.nim;
-    var nama = req.body.nama;
-    var jurusan = req.body.jurusan;
+  var id = req.body.id_mahasiswa;
+  var nim = req.body.nim;
+  var nama = req.body.nama;
+  var jurusan = req.body.jurusan;
 
-    connection.query('UPDATE mahasiswa SET nim=?, nama=?, jurusan=? WHERE id_mahasiswa=?', [nim, nama, jurusan, id],
-        function (error, rows, fields) {
-            if (error) {
-                console.log(error);
-            } else {
-                response.ok("berhasil Ubah Data", res)
-            }
-        });
+  connection.query(
+    "UPDATE mahasiswa SET nim=?, nama=?, jurusan=? WHERE id_mahasiswa=?",
+    [nim, nama, jurusan, id],
+    function (error, rows, fields) {
+      if (error) {
+        console.log(error);
+      } else {
+        response.ok("berhasil Ubah Data", res);
+      }
+    }
+  );
 };
 
 //menghapus data berdasarkan id
 exports.hapusmahasiswa = function (req, res) {
-    var id = req.body.id_mahasiswa;
+  var id = req.body.id_mahasiswa;
 
-    connection.query('DELETE FROM mahasiswa WHERE id_mahasiswa=?', [id],
-        function (error, rows, fields) {
-            if (error) {
-                console.log(error);
-            } else {
-                response.ok("Berhasil hapus data", res);
-            }
-        });
+  connection.query(
+    "DELETE FROM mahasiswa WHERE id_mahasiswa=?",
+    [id],
+    function (error, rows, fields) {
+      if (error) {
+        console.log(error);
+      } else {
+        response.ok("Berhasil hapus data", res);
+      }
+    }
+  );
 };
 
 //menampilkan matakuliah group
-exports.tampilgroupmatakuliah = function(req, res){
-    connection.query('SELECT mahasiswa.id_mahasiswa, mahasiswa.nim, mahasiswa.nama, mahasiswa.jurusan, matakuliah.matakuliah, matakuliah.sks FROM krs JOIN matakuliah JOIN mahasiswa WHERE krs.id_matakuliah = matakuliah.id_matakuliah AND krs.id_mahasiswa = mahasiswa.id_mahasiswa ORDER BY mahasiswa.id_mahasiswa',
-    function(error,rows,fields){
-        if(error){
-            console.log(error);
-        }else{
-            response.oknested(rows, res);
-        }
+exports.tampilgroupmatakuliah = function (req, res) {
+  connection.query(
+    "SELECT mahasiswa.id_mahasiswa, mahasiswa.nim, mahasiswa.nama, mahasiswa.jurusan, matakuliah.matakuliah, matakuliah.sks FROM krs JOIN matakuliah JOIN mahasiswa WHERE krs.id_matakuliah = matakuliah.id_matakuliah AND krs.id_mahasiswa = mahasiswa.id_mahasiswa ORDER BY mahasiswa.id_mahasiswa",
+    function (error, rows, fields) {
+      if (error) {
+        console.log(error);
+      } else {
+        response.oknested(rows, res);
+      }
     }
-    )
-}
+  );
+};
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,10 @@
-var connection = require('../koneksi');
-var mysql = require('mysql');
-var md5 = require('md5');
-var response = require('../res');
-var jwt = require('jsonwebtoken');
-var config = require('../config/secret');
-var ip = require('ip');
+var connection = require("../koneksi");
+var mysql = require("mysql");
+var md5 = require("md5");
+var response = require("../res");
+var jwt = require("jsonwebtoken");
+var config = require("../config/secret");
+var ip = require("ip");
 // var nodemailer = require('nodemailer')
 
 // let smtpTransport = nodemailer.createTransport({
@@ -22,108 +22,107 @@ var ip = require('ip');
 // exports.ubahPassword
 
 //controller untuk register
-exports.registrasi = function(req, res){
-    var post = {
-        username: req.body.username,
-        email: req.body.email,
-        password: md5(req.body.password),
-        role: req.body.role,
-        tanggal_daftar : new Date()
-        // isVerified: 0
-    }
+exports.registrasi = function (req, res) {
+  var post = {
+    username: req.body.username,
+    email: req.body.email,
+    password: md5(req.body.password),
+    role: req.body.role,
+    tanggal_daftar: new Date(),
+    // isVerified: 0
+  };
 
-    var query = "SELECT email FROM ?? WHERE ??=?";
-    var table = ["user", "email", post.email];
+  var query = "SELECT email FROM ?? WHERE ??=?";
+  var table = ["user", "email", post.email];
 
-    query = mysql.format(query,table);
+  query = mysql.format(query, table);
 
-    connection.query(query, function(error, rows,){
-        if(error){
+  connection.query(query, function (error, rows) {
+    if (error) {
+      console.log(error);
+    } else {
+      if (rows.length == 0) {
+        var query = "INSERT INTO ?? SET ?";
+        var table = ["user"];
+        query = mysql.format(query, table);
+        connection.query(query, post, function (error, rows) {
+          if (error) {
             console.log(error);
-        }else{
-            if(rows.length == 0){
-                var query = "INSERT INTO ?? SET ?";
-                var table = ["user"];
-                query = mysql.format(query, table);
-                connection.query(query, post, function(error, rows){
-                    if(error){
-                        console.log(error);
-                    }else{
-                        response.ok("Berhasil menambahkan data user baru", res);
-                    }
-                });
-            }else{
-                response.ok("Email sudah terdaftar!", res);
-            }
-        }
-    })
-}
+          } else {
+            response.ok("Berhasil menambahkan data user baru", res);
+          }
+        });
+      } else {
+        response.ok("Email sudah terdaftar!", res);
+      }
+    }
+  });
+};
 
 // controller untuk login
-exports.login = function(req, res) {
-    var post = {
-        password: req.body.password,
-        email: req.body.email
-    }
+exports.login = function (req, res) {
+  var post = {
+    password: req.body.password,
+    email: req.body.email,
+  };
 
-    var query = "SELECT * FROM ?? WHERE ??=? AND ??=?";
-    var table = ["user", "password", md5(post.password), "email", post.email];
+  var query = "SELECT * FROM ?? WHERE ??=? AND ??=?";
+  var table = ["user", "password", md5(post.password), "email", post.email];
 
-    query = mysql.format(query, table);
-    connection.query(query, function (error, rows) {
-        if(error) {
+  query = mysql.format(query, table);
+  connection.query(query, function (error, rows) {
+    if (error) {
+      console.log(error);
+    } else {
+      if (rows.length == 1) {
+        var token = jwt.sign({ rows }, config.secret, {
+          expiresIn: "2400000",
+        });
+        id_user = rows[0].id;
+        //1 tambahan row username
+        username = rows[0].username;
+        //2 tambahan row role
+        role = rows[0].role;
+
+        var expired = 2400000;
+        var isVerified = rows[0].isVerified;
+
+        var data = {
+          id_user: id_user,
+          access_token: token,
+          ip_address: ip.address(),
+        };
+
+        var query = "INSERT INTO ?? SET ?";
+        var table = ["akses_token"];
+
+        query = mysql.format(query, table);
+        connection.query(query, data, function (error, rows) {
+          if (error) {
             console.log(error);
-        }else{
-            if(rows.length == 1) {
-                var token = jwt.sign({ rows }, config.secret, {
-                    expiresIn: '2400000'
-                });
-                id_user = rows[0].id;
-                //1 tambahan row username
-                username = rows[0].username;
-                //2 tambahan row role
-                role = rows[0].role;
+          } else {
+            res.json({
+              success: true,
+              message: "Token JWT tergenerate!",
+              token: token,
+              currUser: data.id_user,
+              //4 tambahkan expired time
+              expires: expired,
+              currUser: data.id_user,
+              user: username,
+              //3 tambahkan role
+              role: role,
+              isVerified: isVerified,
+            });
+          }
+        });
+      } else {
+        res.json({ Error: true, Message: "Email atau Password Salah!" });
+      }
+    }
+  });
+};
 
-                var expired = 2400000
-                var isVerified = rows[0].isVerified
-
-                var data = {
-                    id_user: id_user,
-                    access_token: token,
-                    ip_address: ip.address()
-                }
-
-                var query = "INSERT INTO ?? SET ?";
-                var table = ["akses_token"];
-
-                query = mysql.format(query, table);
-                connection.query(query, data, function (error, rows) {
-                    if(error){
-                        console.log(error);
-                    }else{
-                        res.json({
-                            success: true,
-                            message: 'Token JWT tergenerate!',
-                            token: token,
-                            currUser: data.id_user,
-                            //4 tambahkan expired time
-                            expires: expired,
-                            currUser: data.id_user,
-                            user: username,
-                            //3 tambahkan role
-                            role: role,
-                            isVerified: isVerified
-                        });
-
-                    }
-                });
-            }else{
-                res.json({"Error": true, "Message":"Email atau Password Salah!"});
-            }
-        }
-    });
-}
-
-exports.halamanrahasia = function(req, res){
-    response.ok("Halaman ini hanya untuk user dengan role = 2!",res)
-}
+exports.halamanrahasia = function (req, res) {
+  response.ok("Halaman ini hanya untuk user dengan role = 2!", res);
+};
